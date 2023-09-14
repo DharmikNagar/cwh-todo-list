f5ba96c1a1176fe7bef29777c94d1513f10998c8
DharmikNagar <nagardharmik512@gmail.com>
2023-09-14 09:19:53 +0530

    Angular basic project understanding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HomeComponent,
     BrowserModule,
+    DetailsComponent,
     AppRoutingModule
   ],
   providers: [],
